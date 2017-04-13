b9827515b432d42b8a75a3e933b3330acb521b92
Tim <Tim.mouse@gmail.com>
2017-04-13 22:20:54 +0800

    fix login

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var mysql   = require("mariasql");
+var mysql   = require("mysql");
 var bodyParser  = require("body-parser");
 var md5 = require('MD5');
 var rest = require("./REST.js");
