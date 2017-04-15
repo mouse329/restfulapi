4087761108a5a808d47677ff9b3fe25822d91013
tim <tim.mouse@gmail.com>
2017-04-16 06:09:34 +0800

    ErrorCode

--- REST.js
+++ REST.js
@@ -17,6 +17,7 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
         query = mysql.format(query,table);
         connection.query(query,function(err,rows){
             if(err) {
+                console.log(+ err.code + " ,isFatal - " + err.fatal);
                 res.json({"Error" : true, "Message" : "Error executing MySQL query"});
             } else {
                 res.json({"Error" : false, "Message" : "Success", "Users" : rows});
